5500c5c08c3011ad2cc124ee4b4e40f8f94f66ac
Shawn D Wheeler <swheeler1974@gmail.com>
2024-02-20 06:22:39 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LandedMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
